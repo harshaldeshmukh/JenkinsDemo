5fc5649ae37c39a3cb7ceb9839a7d8e6306073c9
Harshal Deshmukh <harshald184@gmail.com>
2020-08-04 23:00:22 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+From java:8
+copy ./target/DockerDemo-0.0.1-SNAPSHOT.jar DockerDemo-0.0.1-SNAPSHOT.jar
+CMD ["java","-jar","DockerDemo-0.0.1-SNAPSHOT.jar"]
---
+++ src/main/java/com/DockerDemo/ControllerClass.java
@@ -0,0 +1,14 @@
+package com.DockerDemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class ControllerClass {
+	
+	@GetMapping("/")
+	public String name() {
+		return "This is Heloow";
+	}
+
+}
